b0203da9af48155786592175645f317adbd541ea
dibo <henscoper@gmail.com>
2025-08-23 13:53:38 +0200

    Grid View

---
+++ Converters/GridWidthConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace N64RecompLauncher
+{
+    public class GridWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double slotScale)
+            {
+                return Math.Max(120, slotScale * 1.33);
+            }
+            return 120;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Converters/SlotScaleToGridConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace N64RecompLauncher
+{
+    public class SlotScaleToGridConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double slotScale && parameter is string mode)
+            {
+                bool isGridMode = slotScale > 120;
+
+                if (mode == "Grid")
+                    return isGridMode ? Visibility.Visible : Visibility.Collapsed;
+                else
+                    return isGridMode ? Visibility.Collapsed : Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
